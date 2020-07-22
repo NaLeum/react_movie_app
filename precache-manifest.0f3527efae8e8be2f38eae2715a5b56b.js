8aa2ff90bb64e05598101fc3f8336c7bbebbdb25
NaLeum <wognsdl@likelion.org>
2020-07-22 17:22:53 +0900

    Updates

---
+++ precache-manifest.0f3527efae8e8be2f38eae2715a5b56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09d4e3461507c8f6262051fcf6af866",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "76a64883ebb717efc432",
+    "url": "/react_movie_app/static/css/main.ec23c3db.chunk.css"
+  },
+  {
+    "revision": "fe56c5fc6526f79aa64c",
+    "url": "/react_movie_app/static/js/2.994be385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.994be385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a64883ebb717efc432",
+    "url": "/react_movie_app/static/js/main.b0bc7f46.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  }
+]);
