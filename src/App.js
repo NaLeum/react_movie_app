fe29d1a27135785e6ea696a8005e2d13ed31162a
NaLeum <wognsdl@likelion.org>
2020-07-22 17:16:59 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,66 +1,65 @@
 import React from 'react';
-import PropType from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
 
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-    , rating: 4.5
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-    , rating: 4.7
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-    , rating: 4.8
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-    , rating: 4.2
+class App extends React.Component {
+  // constructor(prop) {
+  //   super(prop)
+  //   console.log('hello')
+  // }
+  // componentDidMount() {
+  //   console.log('component rendered');
+  // }
+  // componentDidUpdate() {
+  //   console.log('component upadate');
+  // }
+  // componentWillUnmount() {
+  //   console.log('goodbye')
+  // }
+  state = {
+    isLoading: true,
+    movies: []
   }
-];
-function Food({ name, picture, rating }) {
-  return <div>
-    <h1> I like {name}</h1>
-    <h4> {rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-}
-Food.propType = {
-  name: PropType.string.isRequired,
-  picture: PropType.string.isRequired,
-  rating: PropType.number.isRequired
-}
-function App() {
-  return (
-    <div>
-      <h1> Hello!</h1>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>);
-
-
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log(movies);
+    this.setState({ movies, isLoading: false })
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    // console.log('rendering')
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+      </section>
+    )
+  };
 }
 
 export default App; 
