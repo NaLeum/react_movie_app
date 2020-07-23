5e1ed8597b296f5877edec3c3476f35785ce0df9
NaLeum <wognsdl@likelion.org>
2020-07-23 17:10:35 +0900

    edit detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Detail.css';
 class Detail extends React.Component {
     componentDidMount() {
         const { location, history } = this.props;
@@ -11,9 +11,23 @@ class Detail extends React.Component {
 
     render() {
         const { location } = this.props;
-        console.log(location);
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="detail__container">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                    <div className="detail__data">
+                        <h3 className="detail__title">{location.state.title}</h3>
+                        <h5 className="detail__year">{location.state.year}</h5>
+                        <ul className="detail__genres">
+                            {location.state.genres.map((genre, index) => (
+                                <li key={index} className="genres__genre">{genre}</li>
+                            ))}
+                        </ul>
+                        <p className="detail__summary">{location.state.summary}</p>
+
+                    </div>
+                </div>
+            );
         } else {
             return null;
         };
